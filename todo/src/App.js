f99b3f968a52957c79e763801f32b65c1d984241
tanaka <tanaka@tanakanoMacBook-Air.local>
2022-11-22 00:45:59 +0900

    add: ToDoアプリの作成

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+import ToDoApp from './components/ToDoApp'
+import './App.css'
+
+function App () {
+  return (
+    <div className="container is-fluid">
+      <ToDoApp />
+    </div>
+  )
+}
+
+export default App
